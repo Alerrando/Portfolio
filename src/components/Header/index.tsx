eba5ae2c324fc89cf24b558ef1abd2dfb06d8cad
Alerrando Breno <alerrando2@gmail.com>
2022-08-31 19:43:23 -0300

    completed menu function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,20 +5,27 @@ import {
   X,
   YoutubeLogo,
 } from "phosphor-react";
+import { useState } from "react";
 import logo from "./image/favicon.ico";
 
 export function Header() {
+  const [menu, setMenu] = useState("hidden");
+
+  function handleMenu(){
+    menu == "block" ? setMenu("hidden") : setMenu("block");
+  }
+
   return (
-    <header className="w-full bg-princ flex justify-center">
+    <header className="w-full bg-princ flex justify-center fixed">
       <div className="w-[92%] alinhamento justify-between py-2">
         <div className="w-[45px] h-[45px]">
-          <img src={logo} alt="icone do site" className="w-full h-full" />
+          <img src={logo} alt="icone do site" className="w-full h-full cursor-pointer" />
         </div>
 
-        <nav className="absolute h-screen w-48 block top-0 right-0 md:relative md:h-full md:flex md:w-auto">
+        <nav className={`absolute h-screen w-48 top-0 right-0 md:relative md:h-full md:flex md:w-auto transition-all ${menu}`}>
           <div className="w-full h-full grid grid-rows-sidebar bg-[#2e3238] md:bg-transparent md:flex">
             <header className="alinhamento justify-end mr-4 cursor-pointer md:hidden">
-              <X size={32} className="text-white" weight="bold" />
+              <X size={32} className="text-white" weight="bold" onClick={() => handleMenu()} />
             </header>
 
             <main className="h-52 flex mx-auto mt-8 mb-20 md:items-center md:m-0 md:h-full">
@@ -61,7 +68,7 @@ export function Header() {
             </footer>
           </div>
         </nav>
-        <List className="block md:hidden" size={32} weight="bold" />
+        <List className="block cursor-pointer text-white md:hidden" size={32} weight="bold" onClick={() => handleMenu()} />
       </div>
     </header>
   );
