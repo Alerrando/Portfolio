418b13548c6eb97b731fe8c1d0301de931b613e1
Alerrando Breno <alerrando2@gmail.com>
2022-08-31 19:22:51 -0300

    Nav style for mobile completed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,67 @@
-import logo from './image/favicon.ico';
+import {
+  FacebookLogo,
+  List,
+  TwitterLogo,
+  X,
+  YoutubeLogo,
+} from "phosphor-react";
+import logo from "./image/favicon.ico";
 
 export function Header() {
   return (
     <header className="w-full bg-princ flex justify-center">
       <div className="w-[92%] alinhamento justify-between py-2">
         <div className="w-[45px] h-[45px]">
-          <img
-            src={logo}
-            alt="icone do site"
-            className="w-full h-full"
-          />
+          <img src={logo} alt="icone do site" className="w-full h-full" />
         </div>
 
-        <nav>
-            <ul className="alinhamento gap-3 text-white">
-                <li>
-                    <a href="#">Projetos</a>
+        <nav className="absolute h-screen w-48 block top-0 right-0 md:relative md:h-full md:flex md:w-auto">
+          <div className="w-full h-full grid grid-rows-sidebar bg-[#2e3238] md:bg-transparent md:flex">
+            <header className="alinhamento justify-end mr-4 cursor-pointer md:hidden">
+              <X size={32} className="text-white" weight="bold" />
+            </header>
+
+            <main className="h-52 flex mx-auto mt-8 mb-20 md:items-center md:m-0 md:h-full">
+              <ul className=" block md:flex md:gap-6 text-white">
+                <li className="mb-6 hover:text-red-500 transition-colors text-xl md:mb-0">
+                  <a href="#">Projetos</a>
                 </li>
-                <li>
-                    <a href="#">Sobre</a>
+                <li className="mb-6 hover:text-red-500 transition-colors text-xl md:mb-0">
+                  <a href="#">Sobre</a>
                 </li>
-                <li>
-                    <a href="#">Contato</a>
+                <li className="mb-6 hover:text-red-500 transition-colors text-xl md:mb-0">
+                  <a href="#">Contato</a>
                 </li>
-                
-            </ul>
+              </ul>
+            </main>
+
+            <footer className="w-full md:hidden">
+              <div className="alinhamento justify-around">
+                <div className="w-10 h-10 rounded-full">
+                  <FacebookLogo
+                    className="h-[80%] w-[80%] text-white cursor-pointer transition-colors hover:text-[#1877F2]"
+                    weight="bold"
+                  />
+                </div>
+
+                <div className="w-10 h-10 rounded-full">
+                  <YoutubeLogo
+                    className="h-[80%] w-[80%] text-white cursor-pointer transition-colors hover:text-[#FF0000]"
+                    weight="bold"
+                  />
+                </div>
+
+                <div className="w-10 h-10 rounded-full">
+                  <TwitterLogo
+                    className="h-[80%] w-[80%] text-white cursor-pointer transition-colors hover:text-[#1A8CD8]"
+                    weight="bold"
+                  />
+                </div>
+              </div>
+            </footer>
+          </div>
         </nav>
+        <List className="block md:hidden" size={32} weight="bold" />
       </div>
     </header>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     extend: {
       colors:{
         'princ': "#393E46",
-      }
+        'sombreamento': 'rgba(0, 0, 0, 0.5)',
+      },
+      gridTemplateRows: {
+        "sidebar": "10% 70% 15%",
+      },
     },
   },
   plugins: [],
