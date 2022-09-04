690e27c4177f5d06052b6075416405ffb666269a
Alerrando Breno <alerrando2@gmail.com>
2022-09-04 16:47:50 -0300

    add o form

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -9,6 +9,17 @@ export function Contato(){
                 <div className="block md:flex">
                     <div className="text-white">
                         <h2 className="font-roboto text-2xl font-bold">Entre em contato</h2>
+
+                        <form className="block w-52 py-6">
+                            <label>Digite seu nome</label>
+                            <input type="text" placeholder="Digite seu nome..." className="mb-3" />
+
+                            <label>Digite seu email</label>
+                            <input type="email" placeholder="Digite seu email..." className="mb-3" />
+
+                            <label>Digite sua mensagem</label>
+                            <textarea cols={30} rows={10}></textarea>
+                        </form>
                     </div>
                 </div>
             </div>
