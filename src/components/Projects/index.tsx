9dbcb70b75452a54b0b3e9cec7dc54f08c2af46f
Alerrando <alerrando2@gmail.com>
2024-05-28 14:19:29 -0300

    feat: add projects layout different

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,147 +1,167 @@
 import { GithubLogo, Monitor } from "phosphor-react";
 import { Title } from "../Title";
+import { Key } from "react";
 
 type ProjectsTypes = {
   title: string,
-  subTitle: string,
+  description: string,
   img: string,
   alt: string,
   githubSite: string,
   desktopSite: string,
-  projectFilter: string,
+  projectFilter: string[],
 }
 
 const projects: ProjectsTypes[] = [
   {
     title: "Clipboard landing page master",
-    subTitle: "Projeto do site Front End Mentor feito com Html e Css",
+    description: "Projeto do site Front End Mentor feito com Html e Css",
     img: "Projetos/projeto1.png",
     alt: "Projeto Loja",
     githubSite: "https://github.com/Alerrando/clipboard-landing-page-master",
     desktopSite:
       "https://alerrando.github.io/clipboard-landing-page-master/index.html",
-    projectFilter: "Html/Css/Js",
+    projectFilter: ["Html", "Css", "Javascript"],
   },
   {
     title: "Previsão do Tempo",
-    subTitle: "Projeto consumindo API com Html, Css e Javascript",
+    description: "Projeto consumindo API com Html, Css e Javascript",
     img: "Projetos/projeto2.png",
     alt: "Projeto de previsão do tempo",
     githubSite: "https://github.com/Alerrando/Previsao-do-Tempo",
     desktopSite: "https://alerrando.github.io/Previsao-do-Tempo/",
-    projectFilter: "Html/Css/Js"
+    projectFilter: ["Html", "Css", "Javascript"],
   },
   {
     title: "Google",
-    subTitle: "Projeto de cópia da Google",
+    description: "Projeto de cópia da Google",
     img: "Projetos/projeto3.png",
     alt: "Projeto Google",
     githubSite: "https://github.com/Alerrando/Google",
     desktopSite: "https://alerrando.github.io/Google/index.html",
-    projectFilter: "Html/Css/Js",
+    projectFilter: ["Html", "Css", "Javascript"],
   },
   {
     title: "Crud Hotel",
-    subTitle: "Projeto de um Crud usando ReactJS",
+    description: "Projeto de um Crud usando ReactJS",
     img: "Projetos/projeto4.png",
     alt: "Projeto Crud Hotel",
     githubSite: "https://github.com/Alerrando/Crud-Hotel",
     desktopSite: "https://crud-hotel.vercel.app/",
-    projectFilter: "React",
+    projectFilter: ["React"],
   },
   {
     title: "Loja - TechSoft",
-    subTitle: "Projeto da Loja TechSoft refeita com ReactJS",
+    description: "Projeto da Loja TechSoft refeita com ReactJS",
     img: "Projetos/projeto5.png",
     alt: "Projeto Loja com ReactJS",
     githubSite: "https://github.com/Alerrando/Loja-ReactJs",
     desktopSite: "https://loja-react-js.vercel.app/",
-    projectFilter: "React",
+    projectFilter: ["React"],
   },
   {
     title: "Instagram",
-    subTitle: "Projeto do Instagram feito com ReactJS",
+    description: "Projeto do Instagram feito com ReactJS",
     img: "Projetos/projeto6.png",
     alt: "Projeto do Instagram feito com ReactJS",
     githubSite: "https://github.com/Alerrando/Instagram-Clone",
     desktopSite: "https://instagram-clone-alerrando.vercel.app/",
-    projectFilter: "React",
+    projectFilter: ["React", "Typescript", "TailwindCSS"],
   },
   {
     title: "Pokedex",
-    subTitle: "Projeto usando a API PokeAPI",
+    description: "Projeto usando a API PokeAPI",
     img: "Projetos/projeto7.png",
     alt: "Projeto de Pokedex",
     githubSite: "https://github.com/Alerrando/Pokedex",
     desktopSite: "https://pokedex-alerrando.vercel.app/",
-    projectFilter: "React",
+    projectFilter: ["React", "Typescript"],
   },
 
   {
     title: "Previsão do Tempo",
-    subTitle: "Projeto Previsão do Tempo",
+    description: "Projeto Previsão do Tempo",
     img: "Projetos/projeto8.png",
     alt: "Projeto de Previsão do Tempo",
     githubSite: "https://github.com/Alerrando/Previsao-do-Tempo-ReactJS",
     desktopSite: "https://previsao-do-tempo-reactjs-alerrando.vercel.app/",
-    projectFilter: "React",
+    projectFilter: ["React", "Typescript"],
   },
 
   {
     title: "Sistema de Gerenciamento Escolar",
-    subTitle: "Projeto Sistema de Gerenciamento Escolar com ReactJS e Spring Boot", 
+    description: "Projeto Sistema de Gerenciamento Escolar com ReactJS e Spring Boot", 
     img: "Projetos/projeto9.png",
     alt: "Projeto de gerenciamento escolar",
     githubSite: "https://github.com/Alerrando/sistema-seduc",
     desktopSite: "https://sistema-seduc.vercel.app",
-    projectFilter: "React",
+    projectFilter: ["NextJs", "Typescript", "TailwindCSS"],
   },
 
   {
     title: "Clone do Notion",
-    subTitle: "Projeto Notion Clone feito com ReactJS",
+    description: "Projeto Notion Clone feito com ReactJS",
     img: "Projetos/projeto10.png",
     alt: "Projeto do notion clone",
     githubSite: "https://github.com/Alerrando/Notion-Clone",
     desktopSite: "https://notion-clone-alerrando.vercel.app",
-    projectFilter: "React",
+    projectFilter: ["React"],
   },
 ];
 
 export function Projetos() {
 
   return (
-    <section className="bg-zinc-900 py-4">
+    <section className="py-4">
       <a id="projects"></a>
       <div className="max-w-[90%] flex flex-col items-start justify-center gap-10 mx-auto py-6">
         <Title title={"Projetos"} />
 
-        <div className="w-full grid grid-cols-projects-mobile md:grid-cols-projects-web gap-6">
-          {projects.map((projeto: ProjectsTypes, index: number) => (
-            <div className="max-h-[350px] md:max-h-[500px] relative text-center group rounded-xl" key={index}>
-              <div className="w-full h-full">
-                <img
-                  src={projeto.img}
-                  alt={projeto.alt}
-                  className="rounded-xl w-full h-full"
-                />
+        <div className="w-full flex gap-6 overflow-x-auto">
+          {projects.map((projeto: ProjectsTypes, index: Key) => (
+            <div className="min-w-full flex flex-col gap-12">
+              <div className="w-full h-[280px] flex gap-24" key={index}>
+                <aside className="w-full h-full">
+                  <img src={projeto.img} alt={`projetc-${projeto.title}`} className="w-full h-full" />
+                </aside>
+
+                <div className="w-full h-full flex flex-col gap-7 items-center justify-start">
+                  <div className="flex flex-col gap-6">
+                    <h2 className="text-4xl font-bold">{projeto.title}</h2>
+                    
+                    <div className="flex gap-3">
+                      {projeto.projectFilter.map((category: string, indexCategory: Key) => (
+                        <div className="text-white bg-[#26BBA5] border border-[#22C55E] font-bold  px-3 py-2 rounded-lg">
+                          {category}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+
+                  <span>{projeto.description}</span>
+                </div>
               </div>
 
-              <div className="absolute top-[9%] inset-x-0 flex items-start justify-center gap-16 text-white md:hidden md:group-hover:flex transition-all">
-                <a href={projeto.githubSite} target="_blank">
-                  <GithubLogo className="w-6 h-6 md:h-7 md:w-7 text-black" />
-                </a>
-                <a href={projeto.desktopSite} target="_blank">
-                  <Monitor className="w-6 h-6 md:h-7 md:w-7 text-black" />
-                </a>
-              </div>
-              <div className="absolute w-full border-none outline-none ease-in-out bottom-0 py-4 md:hidden md:group-hover:block transition-all">
-                <h2 className="font-roboto text-xl md:text-2xl font-bold text-[#20b2aa]">
-                  {projeto.title}
-                </h2>
-                <span className="font-playfair font-bold text-xs md:text-sm text-orange-500">
-                  {projeto.subTitle}
-                </span>
+              <div className="w-full h-[280px] flex flex-row-reverse gap-24" key={index}>
+                <aside className="w-full h-full">
+                  <img src={projeto.img} alt={`projetc-${projeto.title}`} className="w-full h-full" />
+                </aside>
+
+                <div className="w-full h-full flex flex-col gap-7 items-center justify-start">
+                  <div className="flex flex-col gap-6">
+                    <h2 className="text-4xl font-bold">{projeto.title}</h2>
+                    
+                    <div className="flex gap-3">
+                      {projeto.projectFilter.map((category: string, indexCategory: Key) => (
+                        <div className="text-white bg-[#26BBA5] border border-[#22C55E] font-bold  px-3 py-2 rounded-lg">
+                          {category}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+
+                  <span>{projeto.description}</span>
+                </div>
               </div>
             </div>
           ))}
